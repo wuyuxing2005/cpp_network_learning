5c0d2e6b6f41b9dde79166255b2dcc363594f540
zhangda <2465692164@qq.com>
2026-03-11 09:37:16 +0800

    begin day07 Acceptor

--- Server.h
+++ Server.h
@@ -7,6 +7,10 @@
 #include "set_noblocking.h"
 #include <cstring>
 #define MAX_BUFFER_SIZE 1024
+/*
+以注意到当前Server类已经与event检测，注册，分发完全分离了，只关注于服务器socket处理与服务器逻辑（echo client信息与new connection建立）
+
+*/
 class Server
 {
 private:
